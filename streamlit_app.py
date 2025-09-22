ad16b53f3b3f9d5c8a96280e87b2a494b801d4b3
Leoson Hoay <leosonhoay@gmail.com>
2025-09-22 00:59:33 +0000

    removed truncation

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ else:
             st.secrets["gcp_service_account"]
         )
         bigquery_client = bigquery.Client(credentials=credentials)             
-        QUERY = response[6:-3]
+        QUERY = response
         print(QUERY)
         #Write Query on BQ
         #QUERY = f"SELECT * FROM {bigquery_table_name} LIMIT 10"
